c4735432208062ccd109fc30df4f282c9d626cb4
Meacha1 <meachattd@gmail.com>
2023-01-19 08:41:13 -0800

    commit message

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 
+/**
+ * bubble_sort - sort the array and print each step
+ * @size: size of the array
+ * @array: the array to be sorted
+ */
+
 void bubble_sort(int *array, size_t size)
 {
-    size_t i,j,k;
-    
-    for(k = 0; k < size; k++)
-    {
-        for(i = 0; i < size ; i++)
-        {
-            if (array[i] > array[i+1])
-                {
-                    j = array[i];
-                    array[i] = array[i+1];
-                    array[i+1] = j;
-                    print_array(array, size);
-                }
-        }
-    }
-}
-void print_array(int *array, size_t size)
-{
-    size_t i;
-    for(i = 0; i < size; i++)
-    printf("%d ", array[i]);
-    printf("\n");
+	size_t i, j, k;
+
+	for (k = 0; k < size; k++)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				j = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = j;
+				print_array(array, size);
+			}
+		}
+	}
 }
---
+++ print_array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * print_array - print all the elements in the array
+ * @array: the array to be sorted
+ * @size: size of the arrat
+ */
+
+void print_array(int *array, size_t size)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		printf("%d ", array[i]);
+	printf("\n");
+}
