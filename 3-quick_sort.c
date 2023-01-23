2f5f1cb7d396ef46f51774c94e1be398abc997ca
Meacha1 <meachattd@gmail.com>
2023-01-23 03:23:40 -0800

    commit message

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,48 +1,101 @@
 #include "sort.h"
 
+/**
+ * quick_sort - Function that sorts an array based on
+ * quick sort algorithm
+ * @array: Array to be sorted
+ * @size: Size of array
+ * Return: 0
+ */
 void quick_sort(int *array, size_t size)
 {
-    quick_sort_recursion(array, 0, size - 1);
+	size_t pivot;
+
+	if (!array || size < 2)
+		return;
+
+	print_sort(array, size, 1);
+
+	/* partition and get pivot index */
+	pivot = partition(array, size);
+
+	/* repeat for left of index */
+	quick_sort(array, pivot);
+	/* repeat for index and right */
+	quick_sort(array + pivot, size - pivot);
 }
-int array_size(int *array)
+
+/**
+ * swap - Function that swaps two values
+ *
+ * @a: Fisrt value
+ * @b: Second value
+ * Return: 0
+ */
+void swap(int *a, int *b)
 {
-    int i;
-    for (i = 0; array[i]; i++)
-    ;
-    return i - 3;
+	int tmp;
+
+	tmp = *b;
+	*b = *a;
+	*a = tmp;
 }
-int quick_sort_recursion(int *array, int low, int high)
+
+/**
+ * partition - Function that sets the pivot for quick_sort
+ *
+ * @array: Array to partition
+ * @size: Size of array
+ * Return: (i + 1)
+ */
+size_t partition(int array[], size_t size)
 {
-    if (low < high)
-    {
-        int pivot_index = partition(array,low, high);
-        quick_sort_recursion(array, low, pivot_index - 1);
-        quick_sort_recursion(array, pivot_index + 1, high);
-       
-    }
+	int pivot;
+	size_t i = -1;
+	size_t j;
+
+	if (!array || size < 2)
+		return (0);
+
+	pivot = array[size - 1];
+
+	for (j = 0; j < size - 1; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_sort(array, size, 0);
+			}
+		}
+	}
+	if (i + 1 != size - 1)
+	{
+		swap(&array[i + 1], &array[size - 1]);
+		print_sort(array, size, 0);
+	}
+	return (i + 1);
 }
-int partition(int *array, int low, int high)
+
+/**
+ * print_sort - Function that prints as it should
+ * @array: Array to be printed
+ * @size: Size of array
+ * @init: Should initialize array
+ * Return: 0
+ */
+void print_sort(int array[], size_t size, int init)
 {
-    int size = high - low + 1;
-    int i, j, k, tmp;
-    i = low;
-    j = high - 1;
-    int n = array_size(array);
-    for (k = low; k < size + low - 1; k++)
-    {
-        if (array[k] > array[high] && j > 0)
-        j--;
-        else if(array[k] < array[high])
-        {
-            tmp = array[k];
-            array[k] = array[i];
-            array[i] = tmp;
-            i++;
-        }
-    }
-    tmp = array[i];
-    array[i] = array[high];
-    array[high] = tmp;
-    print_array(array, n);
-    return i;
+	static int *p = (void *)0;
+	static size_t s;
+
+	if (!p && init)
+	{
+		p = array;
+		s = size;
+	}
+	if (!init)
+		print_array(p, s);
 }
